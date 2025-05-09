47b6a166d9fef5a925d6a5572352b2cdb420f578
1Nitro1 <107763540+1Nitro1@users.noreply.github.com>
2025-05-08 23:04:59 -0500

    Update 004.js

--- static/assets/js/004.js
+++ static/assets/js/004.js
@@ -175,16 +175,11 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
+  "G- 3.0?",
+  "New Proxy!",
+  "Made by 1Nitro1/Dan",
   "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
   "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
